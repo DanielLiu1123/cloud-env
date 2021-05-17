56cc9e1fa6de21cfdc25d458c5627dea7947353f
科长 <oncwnuBAY4fNkHMgR8_DusDNf5HU@git.weixin.qq.com>
2021-05-17 18:10:53 +0800

    nacos config 新增给指定配置添加监听器

--- demo-nacos-config/src/main/java/cn/liumouren/example/config/ConfigApp.java
+++ demo-nacos-config/src/main/java/cn/liumouren/example/config/ConfigApp.java
@@ -1,7 +1,14 @@
 package cn.liumouren.example.config;
 
+import com.alibaba.cloud.nacos.NacosConfigManager;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.AbstractListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author Daniel Liu
@@ -11,4 +18,33 @@ public class ConfigApp {
     public static void main(String[] args) {
         SpringApplication.run(ConfigApp.class, args);
     }
+
+
+    @Configuration
+    static class Runner implements ApplicationRunner {
+        @Autowired
+        private NacosConfigManager nacosConfigManager;
+
+        @Override
+        public void run(ApplicationArguments args) throws Exception {
+            ConfigService configService = nacosConfigManager.getConfigService();
+            // 给指定配置添加监听器
+            configService.addListener("demo-nacos-config.yaml", "DEFAULT_GROUP", new AbstractListener() {
+
+                /**
+                 * 使用最新的配置数据进行回调。
+                 *
+                 * @param configInfo 在nacos服务器中的最新内容
+                 */
+                @Override
+                public void receiveConfigInfo(String configInfo) {
+                    System.out.println("=========================================");
+                    System.out.println(configInfo);
+                    System.out.println("=========================================");
+                }
+            });
+        }
+
+    }
+
 }
